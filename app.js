a694990937d328ed4dc3d16fefd9aa808afd046a
ArjunMagar <arjunmagar6820@gmail.com>
2023-12-12 12:02:29 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+
+
+// database connection 
+require("./model/index")
+
+app.get("/",(req,res)=>{
+    res.send("I am live")
+})
+const PORT = process.env.PORT || 4000
+app.listen(PORT,()=>{
+    console.log(`Server has started at port ${ PORT}`)
+})
